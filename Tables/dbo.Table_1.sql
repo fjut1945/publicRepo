8b54e20f0cb9047ebb9df687d1be382b10c62805
fjut1945 <>
2014-10-30 20:19:53 +0100

    Commited with ApexSQL Source Control

--- Tables/dbo.Table_1.sql
+++ Tables/dbo.Table_1.sql
@@ -4,7 +4,9 @@ SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[Table_1] (
 		[asdasasdda]     [nchar](12) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-		[123sada123]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+		[123sada123]     [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[dfjhdfgj]       [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[fdgjfdj]        [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Table_1] SET (LOCK_ESCALATION = TABLE)
