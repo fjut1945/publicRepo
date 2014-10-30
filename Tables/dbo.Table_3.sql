22532efa25d693ef40f5884fc8004e255225a48d
fjut1945 <>
2014-10-30 20:39:58 +0100

    Commited with ApexSQL Source Control

--- Tables/dbo.Table_3.sql
+++ Tables/dbo.Table_3.sql
@@ -3,8 +3,9 @@ SET QUOTED_IDENTIFIER ON
 SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[Table_3] (
-		[sgf]      [smallint] NULL,
-		[dfhe]     [nvarchar](max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+		[sgf]       [smallint] NULL,
+		[dfhe]      [nvarchar](max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[movie]     [smalldatetime] NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Table_3] SET (LOCK_ESCALATION = TABLE)
